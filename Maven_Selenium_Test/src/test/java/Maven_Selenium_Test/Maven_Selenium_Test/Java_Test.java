69ae5bc59f7b53f624727ab926c265231c6acb39
Mirza <mirza99120@gmail.com>
2020-12-19 19:54:05 -0500

    Update Java_Test.java
    code updated

--- Maven_Selenium_Test/src/test/java/Maven_Selenium_Test/Maven_Selenium_Test/Java_Test.java
+++ Maven_Selenium_Test/src/test/java/Maven_Selenium_Test/Maven_Selenium_Test/Java_Test.java
@@ -24,7 +24,7 @@ public class Java_Test {
         JavascriptExecutor js = (JavascriptExecutor)driver;  
         js.executeScript("scrollBy(0, 5000)");   
           
-         // Click on the Search button. Code Update #7  
+         // Click on the Search button. Code Update #8  
         driver.findElement(By.linkText("Core Java")).click();   
         
 	}
